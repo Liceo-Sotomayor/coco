7b5a64355b772e55eb932a2aca436c676bbe42a0
joaco90 <joaquin.salasurquiza@gmail.com>
2017-06-10 09:58:24 -0400

    mi primer comit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    float grados, radianes;
+    float pi = 3.14;
+    cout << "Ingresa un numero" << endl;
+    cin >> radianes;
+    grados = radianes radianes*180 ;
+    cout <<  radianes << " son " << grados << " grados." << endl;
+}
