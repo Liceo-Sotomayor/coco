74e59a54d65e2a4b50b4f1262aa5364427d9afd3
joaco90 <joaquin.salasurquiza@gmail.com>
2017-07-29 10:06:51 -0400

    juegos de dados

--- rol.cpp
+++ rol.cpp
@@ -1,15 +1,27 @@
-#include <iostream>
-#include <string>
+#include <iostream> 
+#include <cmath>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 int main() {
-    bool esMayor;
-    int a, b;
-    cout << "ingrese un numero" << endl;
-    cin >> a;
-    cout << "ingrese otro numero" << endl;
-    cin >> b;
-    esMayor = a > b;
-    if (esMayor) {
-        cout << "El numero " << a << " es menor que " << b << endl;
+    srand (time(NULL));
+    int arma, escudo, dano;
+    int vida=100;
+    
+    while(vida>0){
+        arma = rand()%6+1;
+        arma +=rand()%6+1;
+        arma += 3;
+
+        escudo = rand()%4+1;
+        escudo +=rand()%4+1; 
+
+        dano = arma - escudo;
+        if (dano> 0) {
+            vida-=dano;
+            cout << " El dano es " << dano << " vida restante " << vida << endl;
+        } else {
+            cout << " Sin dano " << endl;
+        }
     }
 }
